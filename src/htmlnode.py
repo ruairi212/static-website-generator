fb3d389cf5ddc1ec77f9318d89d4f43e20f00a5f
ruairi <ruairitierney212@gmail.com>
2025-07-25 10:15:27 +0100

    added parentnode and unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,8 +20,8 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag},{self.value},{self.children},{self.props})"
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value, children=None, props=None):
-        super().__init__(tag, value, children=None, props)
+    def __init__(self, value,tag=None, children=None, props=None):
+        super().__init__(tag, value ,props, children=None)
         
     def to_html(self):
         if self.value == None:
@@ -29,3 +29,21 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No tag given")
+        if self.children == None:
+            raise ValueError("Children is None")
+        list = []
+        for child in self.children:
+            list.append(child.to_html())
+        list = "".join(list)
+        str = f"<{self.tag}{self.props_to_html()}>{list}</{self.tag}>"
+        return str
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,15 +1,15 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode ,ParentNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
         node = HTMLNode("<html>","Text inside a paragraph",None,{
-    "href": "https://www.google.com",
+    "href":"https://www.google.com",
 }
 )
         self.assertEqual(
-            node.props_to_html(),'href="https://www.google.com "'
+            node.props_to_html(),'href="https://www.google.com"'
         )
 
     def test_values(self):
@@ -32,4 +32,17 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.props,
             None,
+        )
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
         )
