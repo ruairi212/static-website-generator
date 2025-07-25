dae08390a4eea3473c262eaf6014e858c499700b
ruairi <ruairitierney212@gmail.com>
2025-07-25 08:36:28 +0100

    finished the leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,14 +10,22 @@ class HTMLNode:
     def props_to_html(self):
         if self.props == None:
             return ""
-        str = ""
+        list = []
         for key, value in self.props.items():
-            str += f'{key}="{value} "'
+            list.append(f'{key}="{value}"')
+        
+        str =  " ".join(list)
         return str
     
     def __repr__(self):
         return f"HTMLNode({self.tag},{self.value},{self.children},{self.props})"
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, children=None, props=None):
-        super().__init__(tag, value, children, props)
+    def __init__(self, tag=None, value, children=None, props=None):
+        super().__init__(tag, value, children=None, props)
         
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
